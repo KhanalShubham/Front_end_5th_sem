4f289abd83cbcf62cfafcfa76cd5b4a820512ce6
Shubham Khanal <shubhamkhanal593@gmail.com>
2025-06-10 18:14:38 +0545

    authApi.js for connectiong with api.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL=import.meta.env.VITE_API_BASE_URL || 'loclhost:5050/api' //fallback
+const API_URL=import.meta.env.VITE_API_BASE_URL || 
+'localhost:5050/api' //fallback
 
 const instance=axios.create(
     {
